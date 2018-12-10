8e88fc57bb54a1da717b93f9a2017a6cb33d9527
hongdam <nouvenoel@gmail.com>
2018-12-10 11:14:06 +0900

    add stable shot and modify vis

--- full_game_vis.py
+++ full_game_vis.py
@@ -9,6 +9,8 @@ from gym_curling.Simulator import simulate as sim
 
 import time
 
+import visdom
+
 
 def expectation(x, prob):
     return torch.sum(torch.mul(x, prob), dim=(len(prob.shape)-1))
@@ -23,7 +25,7 @@ def max_exp_shot(env, network, device, topk_coors, turn, x):
 
     max_v = -99
     virtual_e = 0
-
+    s = time.time()
     if turn != 15:
 
         # make input
@@ -64,11 +66,83 @@ def max_exp_shot(env, network, device, topk_coors, turn, x):
                 max_v = virtual_e
                 max_action = v_action
                 highlight = i
+    print(time.time()-s)
+    return max_action, highlight
+
+
+def stable_shot(env, network, device, topk_coors, turn):
+    x = torch.linspace(1, 0, 17).to(device)
+
+    # offset_x = [-0.14, -0.07,  0.,  0.07,  0.14]
+    # offset_y = [-0.2, -0.1,  0.,  0.1,  0.2]
+
+    offset_x = [-0.296875 , -0.1484375,  0.       ,  0.1484375,  0.296875 ]
+    offset_y = [-0.705 , -0.3525,  0.    ,  0.3525,  0.705 ]
+
+    if turn != 15:
+        list_for_cat = []
+        for i, v_action in enumerate(topk_coors):
+            for o_y in offset_y:
+                for o_x in offset_x:
+                    o_action = (v_action[0] + o_x, v_action[1] + o_y, v_action[2])
+                    v_state, _ = env.virtual_step(o_action, 0)
+                    list_for_cat.append(utils.to_input(v_state, turn + 1).unsqueeze(0))
+
+        virtual_inputs = torch.cat(list_for_cat, dim=0)
+        with torch.no_grad():
+            _, virtual_v_out = network(virtual_inputs.to(device))
+        virtual_v_out_prob = to_prob(virtual_v_out)
+        virtual_e = expectation(x, virtual_v_out_prob)
+        virtual_e += 8
+        virtual_e = virtual_e.split(len(offset_x) * len(offset_y))
+        # print([(x.mean(), x.std())for x in virtual_e])
+        virtual_e = [x.mean()*(15/(15+x.std())) for x in virtual_e]
+
+        highlight = virtual_e.index(max(virtual_e))
+        max_action = topk_coors[highlight]
+    else:
+        s_time = time.time()
+        score_list = []
+        for i, v_action in enumerate(topk_coors):
+            for o_y in offset_y:
+                for o_x in offset_x:
+                    o_action = (v_action[0] + o_x, v_action[1] + o_y, v_action[2])
+                    v_state, _ = env.virtual_step(o_action, 0.145)
+                    score_list.append(utils.get_score(v_state, 1))
+        score_list = np.asarray(score_list)
+        score_list += 8
+        score_list = score_list
+        score_list = np.split(score_list, 25)
+
+        virtual_e = [x.mean() * (15 / (15 + x.std())) for x in score_list]
+        highlight = virtual_e.index(max(virtual_e))
+        max_action = topk_coors[highlight]
+
+        for i, s in enumerate(score_list):
+            if i == highlight:
+                print("-->", end='')
+            print(s.mean(), s.std())
+            print((15 / (15 + s.std())), '*', s.mean(), '=', (s.mean() * (15 / (15 + s.std()))))
+            print(s)
+            print()
+
+        print(highlight, max_action)
+        print(s_time-time.time())
 
     return max_action, highlight
 
 
 def top_1_shot(policy):
+    action = torch.argmax(policy).item()
+    action = utils.idx_to_action_xy(action)
+
+    return action, 0
+
+def prob_dist(policy):
+    vis = visdom.Visdom()
+    p_out_prob = to_prob(policy)
+    vis.heatmap(p_out_prob.reshape((2, 32, 32))[0], opts=dict(title='0'))
+    vis.heatmap(p_out_prob.reshape((2, 32, 32))[1], opts=dict(title='1'))
     action = torch.argmax(policy).item()
     action = utils.idx_to_action_xy(action)
 
@@ -96,7 +170,7 @@ def main():
 
     x = torch.FloatTensor(range(-8, 8 + 1)).to(device)
 
-    env = Curling(0.145)
+    env = Curling(0.3)
 
     rewards = []
     for _ in range(number_of_games):
@@ -113,9 +187,16 @@ def main():
                 topk_actions_idx2coor.append(t_a)
 
             if t % 2 == 1:
-                action, highlight = max_exp_shot(env, network, device, topk_actions_idx2coor, t, x)
+                # action, highlight = max_exp_shot(env, network, device, topk_actions_idx2coor, t, x)
+                # action, highlight = top_1_shot(p_out)
+                action, highlight = prob_dist(p_out)
+                # if t == 15:
+                #     action, highlight = stable_shot(env, network, device, topk_actions_idx2coor, t)
+                # else:
+                #     action, highlight = max_exp_shot(env, network, device, topk_actions_idx2coor, t, x)
             else:
-                action, highlight = top_1_shot(p_out)
+                action, highlight = prob_dist(p_out)
+                # action, highlight = top_1_shot(p_out)
             state, reward, done = env.step(action)
             env.render(True, topk_actions_idx2coor, highlight)
 
--- gym_curling/env.py
+++ gym_curling/env.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 class Curling:
 
     def __init__(self, uncertainty):
+        # back line: 0
+        self._y_lim = 0
+
         self.state = None
         self.current_turn = None
 
         self.uncertainty = uncertainty
 
         self._fig, self._ax = plt.subplots()
-        self._fig.set_size_inches(4.75 / 2, 11.28 / 2)
+        self._fig.set_size_inches(4.75 / 2, (11.28 - self._y_lim) / 2)
 
         self.x = 0
         self.y = 0
@@ -56,7 +59,7 @@ class Curling:
                 else:
                     color = 'blue'
                     alpha = (1. / i)
-                cir = plt.Circle((coord[0], coord[1]), 0.09, color=color, alpha=alpha)
+                cir = plt.Circle((coord[0], coord[1]), 0.09, color=color, alpha=alpha, zorder=3)
                 if coord[2] == 1:
                     cir.set_edgecolor("black")
                     cir.set_linewidth(2)
@@ -78,28 +81,28 @@ class Curling:
         first = np.asarray([x for x in a[::2] if x[0] != 0 and x[1] != 0 ])
         second = np.asarray([x for x in a[1::2] if x[0] != 0 and x[1] != 0 ])
 
-        cir = plt.Circle((2.375, 4.88), 1.83, color='g', alpha=0.2)
+        cir = plt.Circle((2.375, 4.88), 1.83, color='b', alpha=0.1, zorder=1)
         self._ax.add_artist(cir)
 
-        cir = plt.Circle((2.375, 4.88), 0.5, color='g', alpha=0.2)
+        cir = plt.Circle((2.375, 4.88), 1.22, color='w', alpha=1, zorder=1)
+        self._ax.add_artist(cir)
+
+        cir = plt.Circle((2.375, 4.88), 0.61, color='r', alpha=0.1, zorder=1)
         self._ax.add_artist(cir)
 
         if first.size != 0:
-            self._ax.scatter(first[:, 0], first[:, 1], color='y')
+            self._ax.scatter(first[:, 0], first[:, 1], color='y', zorder=2)
         if second.size != 0:
-            self._ax.scatter(second[:, 0], second[:, 1], color='r')
+            self._ax.scatter(second[:, 0], second[:, 1], color='r', zorder=2)
         self._ax.axis('equal')
 
-
-
-        self._ax.set_ylim(0, 11.28)
+        self._ax.set_ylim(self._y_lim, 11.28)
         self._ax.set_xlim(0, 4.75)
 
-
         if save:
-            plt.savefig('./img/' + str(self.current_turn) + '_{:.3f}_{:.3f}_{:.0f}'.format(self.x,self.y,self.curl) + '.png')
+            plt.savefig('./img/' + str(self.current_turn) + '_{:.3f}_{:.3f}_{:.0f}'.format(self.x, self.y, self.curl) + '.png')
 
-        plt.pause(0.01)
+        # plt.pause(0.01)
         # plt.cla()
 
     @staticmethod
